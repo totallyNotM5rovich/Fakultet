6c3645c35e39c7233bbc428c1a5ed77c62fc8537
Mihael Petrovic <mihael.petrovic1@hotmail.com>
2025-11-10 20:07:29 +0100

    Objekti spremljeni u vaktor i pozivanje metoda iteracijom po vektoru.

--- src/Fakultet.java
+++ src/Fakultet.java
@@ -1,18 +1,16 @@
+import java.util.ArrayList;
+
 public class Fakultet {
     public static void main(String[] args) {
-        Student ali = new Student("Ali", "Abd Elhameed", "musko", 23, 2);
-        Student valentino = new Student("Valentino", "Majrusic", "musko", 24, 3);
+        ArrayList<Osoba> osobe = new ArrayList<>();
+        osobe.add(new Student("Ali", "Abd Elhameed", "musko", 23, 2));
+        osobe.add(new Student("Valentino", "Majrusic", "musko", 24, 3));
+        osobe.add(new Profesor("Franjo", "Sumanovac", "musko", 100, "geofizicka istrazivanja"));
+        osobe.add(new Profesor("Sibila", "Borojevic Sostaric", "zensko", 40, "opticka mineralogija"));
 
-        Profesor franjo = new Profesor("Franjo", "Sumanovac", "musko", 100, "geofizicka istrazivanja");
-        Profesor sibila = new Profesor("Sibila", "Borojevic Sostaric", "zensko", 40, "opticka mineralogija");
-
-        System.out.println(ali.predstavljanje());
-        ali.dohvatiOpisUloge();
-        System.out.println(valentino.predstavljanje());
-        valentino.dohvatiOpisUloge();
-        System.out.println(franjo.predstavljanje());
-        franjo.dohvatiOpisUloge();
-        System.out.println(sibila.predstavljanje());
-        sibila.dohvatiOpisUloge();
+        for (Osoba osoba : osobe) {
+            System.out.println(osoba.predstavljanje());
+            osoba.dohvatiOpisUloge();
+        }
     }
 }
